0ed686cacb12649044f2124b48d0714a331f23c3
duncanstevenson <duncanstevenson@leahurstcollege.ca>
2017-01-18 20:01:48 -0500

    change talon to CAN Talon

--- src/Subsystems/ShooterSubsystem.cpp
+++ src/Subsystems/ShooterSubsystem.cpp
@@ -5,18 +5,39 @@
 #include <Commands/Shooter/ShooterDefault.h>
 #include "ShooterSubsystem.h"
 #include "../RobotMap.h"
+#include "CANTalon.h"
 
 ShooterSubsystem::ShooterSubsystem():
 		Subsystem("ShooterSubsystem")
 {
 	 Turret = new Victor(SHOOTER_TURRET);
-	 Rollers = new TalonSRX(SHOOTER_ROLLER);
+	 Rollers = new CANTalon(SHOOTER_ROLLER);
+
+	 //CANTALLON SETUP//
+	 Rollers->SetFeedbackDevice(CANTalon::CtreMagEncoder_Relative);
+	 Rollers->SetSensorDirection(false);
+	 //peak outputs
+     Rollers->ConfigNominalOutputVoltage(+0.0f, -0.0f);
+     Rollers->ConfigPeakOutputVoltage(+12.0f, 0.0f);
+
+     //PID things
+     Rollers->SelectProfileSlot(0);
+     Rollers->SetF(0.1097);
+	 Rollers->SetP(0.22);
+	 Rollers->SetI(0);
+	 Rollers->SetD(0);
+
 }
 
 void ShooterSubsystem::InitDefaultCommand()
 {
 	// When no other commands are running, we do operator control
 	SetDefaultCommand(new ShooterDefault());
+}
+
+void ShooterSubsystem::SetSpeed(double RPM){
+	Rollers->ApplyControlMode(CANTalon::kSpeedMode);
+	Rollers->SetSetpoint(RPM); /* 1500 RPM in either direction */
 }
 
 
--- src/Subsystems/ShooterSubsystem.h
+++ src/Subsystems/ShooterSubsystem.h
@@ -2,15 +2,17 @@
 
 #include "Commands/Subsystem.h"
 #include "WPILib.h"
+#include "CANTalon.h"
 
 class ShooterSubsystem: public Subsystem
 {
 private:
-	TalonSRX* Rollers;
+	CANTalon* Rollers;
 	Victor* Turret;
 public:
 	ShooterSubsystem();
 	void InitDefaultCommand();
+	void SetSpeed(double RPM);
 };
 
 
