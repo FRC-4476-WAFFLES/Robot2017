ef0689fc535093a5aaf492b6a5223632c5fcbcbe
duncanstevenson <duncanstevenson@leahurstcollege.ca>
2017-01-29 16:10:13 -0500

    shooter stuff

--- src/Subsystems/ShooterSubsystem.cpp
+++ src/Subsystems/ShooterSubsystem.cpp
@@ -19,9 +19,15 @@ ShooterSubsystem::ShooterSubsystem():
 	 //CANTALLON SETUP//
 	 Rollers->SetFeedbackDevice(CANTalon::CtreMagEncoder_Relative);
 	 Rollers->SetSensorDirection(false);
+
+	 Turret->SetFeedbackDevice(CANTalon::CtreMagEncoder_Relative);
+	 Turret->SetSensorDirection(false);
 	 //peak outputs
      Rollers->ConfigNominalOutputVoltage(+0.0f, -0.0f);
      Rollers->ConfigPeakOutputVoltage(+12.0f, 0.0f);
+
+     Turret->ConfigNominalOutputVoltage(+0.0f, -0.0f);
+     Turret->ConfigPeakOutputVoltage(+12.0f, -12.0f);
 
      //PID things
      Rollers->SelectProfileSlot(0);
@@ -29,6 +35,11 @@ ShooterSubsystem::ShooterSubsystem():
 	 Rollers->SetP(0.22);
 	 Rollers->SetI(0);
 	 Rollers->SetD(0);
+
+     Turret->SelectProfileSlot(0);
+     Turret->SetP(0.0022);
+     Turret->SetI(0);
+     Turret->SetD(0);
 
 	 Rollers_Slave->SetControlMode(CANSpeedController::kFollower);
 	 Rollers_Slave->Set(SHOOTER_ROLLER);
@@ -45,6 +56,10 @@ void ShooterSubsystem::SetSpeed(double RPM){
 	Rollers->SetSetpoint(RPM);
 }
 
+void ShooterSubsystem::SetTurret(double TurretAngle){
+	Turret->SetTalonControlMode(CANTalon::kPositionMode);
+	Turret->SetSetpoint(TurretAngle);
+}
 
 
 
