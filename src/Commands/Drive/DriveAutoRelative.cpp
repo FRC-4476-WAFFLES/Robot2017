3080c243ce7b4ec460809c5f2bb0118bbd575790
Ben-Alderson <bennyalderson@gmail.com>
2017-02-03 16:10:32 -0500

    Added to DriveAutoRelative
    Changed distance error

--- src/Commands/Drive/DriveAutoRelative.cpp
+++ src/Commands/Drive/DriveAutoRelative.cpp
@@ -39,28 +39,28 @@ void DriveAutoRelative::Initialize() {
 
 void DriveAutoRelative::Execute() {
 	// Calculate the error on the distance traveled
-	double distanceError = drive->distance() - distance;
+		double distanceError = (drive->distance() - distance)*0.01;
 
-	// Make sure the distance error does not exceed 100%
-	if(distanceError > 1.0) {
-		distanceError = 1.0;
-	}
-	if(distanceError < -1.0) {
-		distanceError = -1.0;
-	}
+		// Make sure the distance error does not exceed 100%
+		if(distanceError > 1.0) {
+			distanceError = 1.0;
+		}
+		if(distanceError < -1.0) {
+			distanceError = -1.0;
+		}
 
-	// Calculate the difference between the current angle and the desired angle
-	double angleError = angle - drive->angle();
+		// Calculate the difference between the current angle and the desired angle
+		double angleError = angle - drive->angle();
 
-	// Set the motors to run
-	drive->drive(0.1*angleError - speed*distanceError, 0.1*angleError - speed*distanceError);
+		// Set the motors to run
+		drive->drive(0.01*angleError + speed*distanceError, -0.01*angleError + speed*distanceError);
 }
 
 // Returns true when the distance is within 200 and the angle is within 5 degrees
 bool DriveAutoRelative::IsFinished() {
 	double distanceError = drive->distance() - distance;
 	double angleError = angle - drive->angle();
-	return distanceError < 200 && distanceError > -200 && angleError < 5.0 && angleError > -5.0;
+	return distanceError < 20 && distanceError > -20 && angleError < 5.0 && angleError > -5.0;
 }
 
 // Stop the motors when this command ends
